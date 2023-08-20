674efec266300bd4454fec19bd5fb5809dd5b1bc
Harsh Singh <harshsingh.js@gmail.com>
2023-08-21 02:17:42 +0530

    Workflows and scripts for landing page

---
+++ build-main.js
@@ -0,0 +1,25 @@
+import { chdir, cwd } from 'process';
+import { execSync } from 'child_process';
+import path from 'path';
+
+const HACKTREE_DIR = path.join(cwd(), 'pages', 'hacktree');
+
+const DIST_DIR = path.join(cwd(), 'dist');
+
+function buildHackTreeLandingPage() {
+    console.log("BUILDING LANDING PAGE");
+
+    chdir(HACKTREE_DIR);
+
+    const output = execSync("fastn build").toString();
+
+    console.log(output);
+
+    const buildDir = path.join(HACKTREE_DIR, '.build');
+
+    execSync(`mv ${buildDir}/* ${DIST_DIR}`);
+
+    console.log("LANDING PAGE BUILD COMPLETE");
+}
+
+buildHackTreeLandingPage();
--- build.js
+++ build.js
@@ -1,11 +1,11 @@
-import { readFile, readdir } from 'fs/promises';
+import { readFile } from 'fs/promises';
 import { chdir, cwd } from 'process';
 import { execSync } from 'child_process';
 import { createFolderHash } from './lib/create-hash.js';
 import { getValidName } from './lib/get-valid-name.js';
 import path from 'path';
 import hasKey from './lib/has.js';
-import { existsSync, rmSync, writeFileSync } from 'fs';
+import { existsSync, readdirSync, rmSync, writeFileSync } from 'fs';
 
 const CURRENT_DIR = cwd();
 
@@ -17,7 +17,7 @@ const OUTPUT_DIR = path.join(CURRENT_DIR, 'dist');
 
 const currentHashMap = JSON.parse((await readFile(HASH_FILE)).toString());
 
-const folders = await readdir(DATA_DIR);
+const folders = readdirSync(DATA_DIR).filter(folder => folder !== 'hacktree');
 
 const folderHash = await Promise.all(folders.map(async folder => {
     console.log(DATA_DIR, folder);
@@ -33,40 +33,42 @@ const pagesToBeUpdated = folderHash.filter(([folder, { hash }]) => !hasKey(curre
 
 const pagesToBeDeleted = Object.entries(currentHashMap).filter(([folder]) => !hasKey(flattenedHashMap, folder));
 
-for(const [folder, { folderName }] of pagesToBeUpdated) {
-    try {
-        const folderPath = path.join(DATA_DIR, folderName);
-
-        chdir(folderPath);
-
-        const output = execSync("fastn build").toString();
-
+function buildAndUpdatePages() {
+    for(const [folder, { folderName }] of pagesToBeUpdated) {
+        try {
+            const folderPath = path.join(DATA_DIR, folderName);
+    
+            chdir(folderPath);
+    
+            const output = execSync("fastn build").toString();
+    
+            console.log(output);
+    
+            const outputDirPath = path.join(OUTPUT_DIR, folder);
+    
+            if(existsSync(outputDirPath)) {
+                rmSync(outputDirPath, { recursive: true, force: true });
+            }
+    
+            const buildDir = path.join(folderPath, '.build');
+    
+            execSync(`mv ${buildDir} ${outputDirPath}`);
+        } catch(e) {
+            console.log(e);
+        }
+    }
+    
+    console.log(pagesToBeDeleted)
+    
+    for(const [folder] of pagesToBeDeleted) {
         const outputDirPath = path.join(OUTPUT_DIR, folder);
-
+    
         if(existsSync(outputDirPath)) {
             rmSync(outputDirPath, { recursive: true, force: true });
         }
-
-        console.log(`BUILD ${folder}:`, output);
-
-        const buildDir = path.join(folderPath, '.build');
-
-        const mvOutput = execSync(`mv ${buildDir} ${outputDirPath}`);
-
-        console.log(mvOutput);
-    } catch(e) {
-        console.log(e);
     }
+    
+    writeFileSync(HASH_FILE, JSON.stringify(flattenedHashMap));
 }
 
-console.log(pagesToBeDeleted)
-
-for(const [folder] of pagesToBeDeleted) {
-    const outputDirPath = path.join(OUTPUT_DIR, folder);
-
-    if(existsSync(outputDirPath)) {
-        rmSync(outputDirPath, { recursive: true, force: true });
-    }
-}
-
-writeFileSync(HASH_FILE, JSON.stringify(flattenedHashMap));
+buildAndUpdatePages();
